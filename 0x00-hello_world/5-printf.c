4dcdf5c84d41fad39e62cdf68b34aaf5ba8be5fb
root <mskander.dridi@gmail.com>
2022-02-03 13:48:00 -0800

    5-hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: skander dridi <4402@holbertonschool.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar, but the outcome is a piece of art,
+ *                
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
