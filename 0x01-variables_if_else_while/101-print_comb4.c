e0c8e9560437031a5f9b7aa5235d71481d1d3805
Skander <62188738+skanderdridi@users.noreply.github.com>
2022-02-07 23:35:48 +0100

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print the combination of three digits
+ * without repeating the same digit
+ *
+ *
+ * Return: 0 (success)
+ **/
+int main(void)
+{
+	int n, c, m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (c = n + 1; c <= 57; c++)
+		{
+			for (m = c + 1; m <= 57; m++)
+			{
+				if (c != n && m != c)
+				{
+					putchar(n);
+					putchar(c);
+					putchar(m);
+					if (!(n == 55 && c == 56 && m == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	
+	return (1);
+}
