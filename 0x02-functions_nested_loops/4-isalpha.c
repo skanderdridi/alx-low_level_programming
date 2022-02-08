8c9f7374f571778b2089bb98f1b490f4a9f021d0
root <mskander.dridi@gmail.com>
2022-02-08 07:55:38 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check if character is alphabetic
+ * @c: character
+ *
+ *
+ * Return: 1 alphabetic 0 otherwise
+ **/
+int _isalpha(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
