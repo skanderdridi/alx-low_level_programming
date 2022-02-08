533231f420c15a1215bcd3b5ac7784d958244f01
Skander <62188738+skanderdridi@users.noreply.github.com>
2022-02-08 18:04:32 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,19 +3,16 @@
 /**
  * _abs -  absolute value
  * @n: print absolute value
- *
- *
  * Return: absolute value
  **/
 int _abs(int n)
 {
-		if (n <= 0)
-		{
-				return (-n);
-		}
+	if (n <= 0)
+	{
+		return (-n);
+	}
 	else
 	{
-				return(n);
+		return (n);
 	}
-	
 }
