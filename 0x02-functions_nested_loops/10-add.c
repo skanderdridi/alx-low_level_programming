27518d8fa81a65517952760ca760db4d8d3c38f0
root <mskander.dridi@gmail.com>
2022-02-08 14:51:51 -0800

    lasttwo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two values
+  * @n1: The first number  computed
+  * @n2: The second number  computed
+  *
+  * Return: Integer value
+  */
+  int add(int n0, int n1)
+{
+	return (n0 + n1);
+}
