d0cc1c223be3f91aaac6f002844e1230c83a7da9
root <mskander.dridi@gmail.com>
2022-02-08 09:02:28 -0800

    6-

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs -  absolute value
+ * @n: print absolute value
+ *
+ *
+ * Return: absolute value
+ **/
+int _abs(int n)
+{
+		if (n <= 0)
+		{
+				return (-n)
+		}
+	else
+	{
+				return(n);
+	}
+	
+}
