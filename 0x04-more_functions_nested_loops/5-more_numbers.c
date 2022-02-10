bb204ce775a80befc0ced30303742bc438f79119
Skander <62188738+skanderdridi@users.noreply.github.com>
2022-02-10 18:41:58 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
-
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
+ * more_numbers - prints numbers(à-14)
+ *
+ * Return: void doesn't return
  */
 void more_numbers(void)
 {
-	int i;
-	int s;
+	int n;
+	int c;
 
-	for (s = 0; s <= 9; s++)
+	for (c = 0; c <= 9; c++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (n = 0; n <= 14; n++)
 		{
-			if (i > 9)
+			if (n > 9)
 			{
-				_putchar(i / 10 + '0');
+				_putchar(n / 10 + '0');
 			}
-			_putchar(i % 10 + '0');
+			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
 	}
