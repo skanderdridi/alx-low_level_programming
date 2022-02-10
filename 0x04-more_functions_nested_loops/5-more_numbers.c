e6ad83dd2b14aa7b4baee8543d9534f8f9ebe4b7
Skander <62188738+skanderdridi@users.noreply.github.com>
2022-02-10 18:39:56 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,18 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int s;
 
-	for (i = 0; i < 10; i++)
+	for (s = 0; s <= 9; s++)
 	{
-		for (j = 0; j < 15; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
