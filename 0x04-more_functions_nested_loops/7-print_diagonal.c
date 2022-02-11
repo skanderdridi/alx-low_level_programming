60b7136d1508fd552eb27f090eaf75a2827d0d33
root <mskander.dridi@gmail.com>
2022-02-11 01:42:54 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * print - print diagonal line '\$' n times
+ * @n: time of n
+ *
+ * Return: void
+ **/
+
+void print_diagonal(int n)
+
+{
+
+	int l, s;
+
+	if (n >= 0)
+	{
+		for (l = 1; l <= n; l++)
+		{
+			for (s = 1; s <= l - 1; s++)
+			_putchar(' ');
+			_putchar('\\');
+			_putchar('$');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
