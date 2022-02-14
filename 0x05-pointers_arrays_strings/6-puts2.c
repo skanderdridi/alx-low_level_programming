36b12d1b6c04464eaeaca8d766953f24f88ffb0e
root <mskander.dridi@gmail.com>
2022-02-14 07:28:16 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* puts2 - main function that prints every character of a string.
+* @str: character.
+* Return: charcters.
+*/
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		if (i % 2 == 0)
+		{
+		_putchar(*str);
+		}
+	str++;
+	i++;
+	}
+_putchar('\n');
+}
