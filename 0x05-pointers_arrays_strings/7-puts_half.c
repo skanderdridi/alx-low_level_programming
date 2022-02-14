3ab4ce84642633b2c4103f5fc4c936df7d6eaf0d
root <mskander.dridi@gmail.com>
2022-02-14 07:42:02 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * puts_half - print the second half of string
+ * @str: string
+ *
+ **/
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	i = i + 1;
+
+	for (i = i / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+#include "main.h"
+
+/**
+ * puts_half - print the second half of string
+ * @str: string
+ *
+ **/
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	i = i + 1;
+
+	for (i = i / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}}
