243564a27424324987099c2c2484d9656dc6b76f
root <mskander.dridi@gmail.com>
2022-02-21 11:42:16 -0800

    3-1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,17 +7,12 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j, b = 0;
-
-	for (; s[i] && s[i] != ' '; i++)
-	{
-		j = 0;
-
-		for (; accept[j]; j++)
-		{
-			if (accept[j] == s[i])
-				b++;
-		}
-	}
-	return (b);
+    unsigned int len =0;
+    if((s1 == NULL) || (accept == NULL))
+        return len;
+    while(*s && strchr(accept,*s++))
+    {
+        len++;
+    }
+    return len;
 }
