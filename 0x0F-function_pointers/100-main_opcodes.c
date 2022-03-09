28b432b6db576822a602c28c9869c2f51ba609b4
root <mskander.dridi@gmail.com>
2022-03-09 11:31:20 -0800

    100-fixsssssss

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,45 @@
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * op_sub -
+ * main - print opcodes of itself
  *
- * Return: 
- */
-int op_sub(int a, int b)
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ *
+ * Return: 0
+ *
+ **/
+int main(int argc, char *argv[])
 {
-	
+
+	unsigned char *ptr;
+	int i, n;
+
+	ptr = (unsigned char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+
+	if (n <= 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", *(ptr + i));
+		if (i != n - 1)
+		putchar(' ');
+	}
+		putchar('\n');
+
+	return (0);
 }
