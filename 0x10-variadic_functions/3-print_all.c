12772c3fd1ffbc8e6d047641c64b6ac03f384b80
Skander <62188738+skanderdridi@users.noreply.github.com>
2022-03-10 15:10:06 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,81 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <stdlib.h>
-
 /**
- * print_all - func to print strings
- * @format: string to be printed
+ * print_chars - prints characters
+ * @str_numbers: parameter
+ * Return: void
+ */
+void print_chars(va_list str_numbers)
+{
+	printf("%c", va_arg(str_numbers, int));
+}
+/**
+ * print_integers - prints integers
+ * @str_numbers: parameter
+ * Return: void
+ */
+void print_integers(va_list str_numbers)
+{
+	printf("%d", va_arg(str_numbers, int));
+}
+/**
+ * print_floats - prints floats
+ * @str_numbers: parameter
+ * Return: void
+ */
+void print_floats(va_list str_numbers)
+{
+	printf("%f", va_arg(str_numbers, double));
+}
+/**
+ * print_string - prints strings
+ * @str_numbers: parameter
+ * @str_numbers: parameter
+ */
+void print_string(va_list str_numbers)
+{
+	char *str = va_arg(str_numbers, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+/**
+ * print_all - prints anything
+ * @format: constant pointer
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	const char *t;
-	char *s;
-	unsigned int i;
-	double d;
-	va_list zug;
-	int len, index;
-	len = (strlen(format) - 1);
-	index = 0;
-	va_start(zug, format);
-	t = format;
-	while (*t != '\0')
+	unsigned int i, j;
+
+	f_t formats[] = {
+		{"c", print_chars},
+		{"i", print_integers},
+		{"f", print_floats},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list str_numbers;
+	char *separate = "";
+
+	va_start(str_numbers, format);
+	i = 0;
+	while (format[i] && format)
 	{
-		switch (*t)
+		j = 0;
+		while (formats[j].symbol != NULL)
 		{
-		case 'c':
-			i = va_arg(zug, int);
-			_putchar(i);
-			break;
-		case 'i':
-			i = va_arg(zug, int);
-			printf("%d", i);
-			break;
-		case 'f':
-			d = va_arg(zug, double);
-			printf("%f", d);
-			break;
-		case 's':
-			s = va_arg(zug, char *);
-			printf("%s", s);
-			break;
+			if (formats[j].symbol[0] == format[i])
+			{
+				printf("%s", separate);
+				formats[j].p(str_numbers);
+				separate = ", ";
+				break;
+			}
+			j++;
 		}
-		if (index < len && (*t == 'i' || *t == 'c' || *t == 'f' || *t == 's'))
-		{
-			printf(", ");
-			index++;
-			t++;
-		}
+		i++;
 	}
-	putchar('\n');
+	printf("\n");
+	va_end(str_numbers);
 }
