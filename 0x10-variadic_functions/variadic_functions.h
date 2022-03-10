aa8657ea3106f575a4f5e078a0a5587d18c4d409
Skander <62188738+skanderdridi@users.noreply.github.com>
2022-03-10 15:09:56 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_comma(const char *c);
+/**
+ * struct format - structure of operator
+ * @symbol: char pointer
+ * @p: pointer to function
+ */
+typedef struct format
+{
+	char *symbol;
+	void (*p)();
+} f_t;
 #endif
