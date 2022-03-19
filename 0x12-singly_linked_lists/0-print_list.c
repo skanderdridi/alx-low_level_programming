79bf14d0e6e38b623b8f902c0ac6e03c19e2a8e8
root <mskander.dridi@gmail.com>
2022-03-19 03:51:27 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: name of the list
@@ -6,12 +7,22 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t error = 0;
 
-	for (i = 0; h; i++)
+	if (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		return (print_list(h->next) + 1);
 	}
-	return (i);
+	else
+	{
+		return (error);
+	}
 }
