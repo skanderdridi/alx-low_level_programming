45ff94546e00a96f83ddbd1f2c37f437595856c9
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-06-20 02:07:55 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef _function_like_macro_h_
-#define _function_like_macro_h_
-
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
-
-#endif 
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
+#endif
