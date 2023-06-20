22994f95bc79ab4c08e49a2a7c44da2e782e39ea
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-06-20 02:18:31 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - is a structure representation of Dog
- * @name: Name of dog
- * @age: Age a puppy
- * @owner: Person responsable of this dog
- * Description: is a structure representation of Dog
+ *struct dog - structure definition of a dog
+ *@name: character string
+ *@age: integer
+ *@owner: character string
  */
-struct dog
+
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-};
+char *name;
+float age;
+char *owner;
+}
+dog_t;
 
-typedef struct dog dog_t;
-
-void init_dog(struct dog *, char *, float, char *);
-void print_dog(struct dog *);
-dog_t *new_dog(char *, float, char *);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+char *_strdup(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
